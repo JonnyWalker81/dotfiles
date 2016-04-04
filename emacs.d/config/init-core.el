d2b8fc39c3f767465dd90e2adc2f6cd7590533bc
JONATHAN ROTHBERG <jonnywalker@JONATHANs-iMac.local>
2016-04-03 21:15:46 -0700

    Setup window for compilation

--- emacs.d/config/init-core.el
+++ emacs.d/config/init-core.el
@@ -13,7 +13,70 @@
 ;; Below line specify the ACTUAL dictionary we use
 (setq ispell-extra-args '("-d en_US"))
 
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
 (global-prettify-symbols-mode +1)
+
+;; (setq split-window-preferred-function 'split-window-horizontally)
+;; (setq split-height-threshold nil)
+;; (setq split-width-threshold most-positive-fixnum)
+
+(defun casey-never-split-a-window
+    "Never, ever split a window.  Why would anyone EVER want you to do that??"
+    nil)
+(setq split-window-preferred-function 'casey-never-split-a-window)
+
+(setq next-line-add-newlines nil)
+(setq-default truncate-lines t)
+(setq truncate-partial-width-windows nil)
+(split-window-horizontally)
+
+;; (add-hook 'compilation-mode-hook (lambda()
+;; 				   (switch-to-buffer "*compilation*")
+;; 				   (other-window 1)))
+
+(defun my-compilation-hook () 
+    "Make sure that the compile window is splitting vertically"
+    (progn
+      (if (not (get-buffer-window "*compilation*"))
+         (progn
+	    (split-window-horizontally)
+	    )
+	  )
+      )
+  )
+
+;; (add-hook 'compilation-mode-hook 'my-compilation-hook)
+
+(defun my-display-buffer (buffer force-other-window)
+  "If BUFFER is visible, select it.
+
+If it's not visible and there's only one window, split the
+current window and select BUFFER in the new window. If the
+current window (before the split) is more than 165 columns wide,
+split horizontally, else split vertically.
+
+If the current buffer contains more than one window, select
+BUFFER in the least recently used window.
+
+This function returns the window which holds BUFFER.
+
+FORCE-OTHER-WINDOW is ignored."
+  (or (get-buffer-window buffer)
+      (if (one-window-p)
+          (let ((new-win (if (> (window-width) 165)
+                             (split-window-horizontally)
+                           (split-window-vertically))))
+            (set-window-buffer new-win buffer)
+            new-win)
+        (let ((new-win (get-lru-window)))
+          (set-window-buffer new-win buffer)
+          new-win))))
+
+(setq display-buffer-function 'my-display-buffer)
+
 
 (set-face-attribute 'default nil
                     :family "Source Code Pro for Powerline" :height 145 :weight 'regular)
